0ea02f1954d48b1dc561dea011ae0bf934ab5192
Christer Holmberg <christer.holmberg@ericsson.com>
2018-05-18 15:53:15 +0200

    Controls text added

--- js/scenes/startscene.js
+++ js/scenes/startscene.js
@@ -8,8 +8,12 @@ class StartScene extends Phaser.Scene {
 
         this.add.image(400, 400, 'tree').setScale(2);
         this.startText = this.add.text(240, 176, 'Mr Appel', { fontSize: '70px', fill: '#FFF' }).setDepth(1); 
-        this.startText = this.add.text(220, 246, 'Press DOWN To Start', { fontSize: '32px', fill: '#FFF' }).setDepth(1); 
-  
+        this.startText = this.add.text(220, 246, 'Press DOWN To Start', { fontSize: '32px', fill: '#FFF' }).setDepth(1);
+        this.startText = this.add.text(320, 300, 'CONTROLS', { fontSize: '32px', fill: '#FFF' }).setDepth(1); 
+        this.startText = this.add.text(220, 330, 'LETF  - Move letf', { fontSize: '32px', fill: '#FFF' }).setDepth(1);
+        this.startText = this.add.text(220, 360, 'RIGHT - Move right', { fontSize: '32px', fill: '#FFF' }).setDepth(1);
+        this.startText = this.add.text(220, 390, 'UP    - Shoot', { fontSize: '32px', fill: '#FFF' }).setDepth(1);
+
         // Key press inputs
         this.cursors = this.input.keyboard.createCursorKeys();
     }
